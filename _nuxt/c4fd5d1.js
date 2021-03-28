ef80f8239ec2a3fe932426dc00445557dca5c75d
dzenjadu3 <81510977+dzenjadu3@users.noreply.github.com>
2021-03-28 18:48:55 +0300

    Add files via upload

---
+++ _nuxt/c4fd5d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{353:function(t,e,n){"use strict";n.r(e);var c={name:"about"},o=n(9),component=Object(o.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"about"},[this._v("about")])}),[],!1,null,"b65cad50",null);e.default=component.exports}}]);
---
+++ _nuxt/d0a8ac3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{358:function(n,t,e){"use strict";e.r(t);var o={mounted:function(){this.$router.push("/shop")}},r=e(9),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
